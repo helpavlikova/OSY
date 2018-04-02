5bbb12c0f414073c8c72b2095b1cc531b008a331
HellmasterPhibi <hellpavlikova@gmail.com>
2018-04-02 12:39:55 +0200

    sequential works on progtest

--- CRig.cpp
+++ CRig.cpp
@@ -1,110 +1,13 @@
-#ifndef __PROGTEST__
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <climits>
-#include <cmath>
-#include <cassert>
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <set>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <pthread.h>
-#include <semaphore.h>
-#include <cstdint>
-#include <array>
-#include <unordered_map>
-#include <unordered_set>
-#include <thread>
-#include <mutex>
-#include <memory>
-#include <condition_variable>
-#include <atomic>
-using namespace std;
-
-
-class CFITCoin;
-class CCVUTCoin;
-class CCustomer;
-
-typedef struct shared_ptr<CFITCoin>                        AFITCoin;
-typedef struct shared_ptr<CCVUTCoin>                       ACVUTCoin;
-typedef struct shared_ptr<CCustomer>                       ACustomer;
-
-//=================================================================================================
-
-class CFITCoin
-{
-  public:
-                             CFITCoin                      ( const vector<uint32_t> & vectors,
-                                                             int               distMax )
-                             : m_Vectors ( vectors ),
-                               m_DistMax ( distMax ),
-                               m_Count ( 0 )
-    {
-    }
-    virtual                  ~CFITCoin                     ( void ) = default;
-    vector<uint32_t>         m_Vectors;
-    int                      m_DistMax;
-    uint64_t                 m_Count;
-};
-
-//=================================================================================================
-
-class CCVUTCoin       
-{
-  public:
-                             CCVUTCoin                     ( const vector<uint8_t> & data,
-                                                             int               distMin,
-                                                             int               distMax )
-                             : m_Data ( data ),
-                               m_DistMin ( distMin ),
-                               m_DistMax ( distMax ),
-                               m_Count ( 0 )
-    {
-    }
-    virtual                  ~CCVUTCoin                    ( void ) = default;
-    vector<uint8_t>          m_Data;
-    int                      m_DistMin;
-    int                      m_DistMax;
-    uint64_t                 m_Count;
-};
-
-//=================================================================================================
-
-class CCustomer
-{
-  public:
-    virtual                  ~CCustomer                    ( void ) = default;
-    virtual AFITCoin         FITCoinGen                    ( void ) = 0;
-    virtual ACVUTCoin        CVUTCoinGen                   ( void ) = 0;
-  
-    virtual void             FITCoinAccept                 ( AFITCoin          x ) = 0;
-    virtual void             CVUTCoinAccept                ( ACVUTCoin         x ) = 0;
-};
-
-//=================================================================================================
-#endif /* __PROGTEST__ */
-
-
 class CRig
 {
   public:
     static void Solve (ACVUTCoin x);
     static void Solve (AFITCoin x);
-    CRig (void) {}
-    ~CRig (void) {}
-    void Start (int thrCnt) {}
-    void Stop (void) {}
-    void AddCustomer (ACustomer c) {}
+    CRig (void);
+    ~CRig (void);
+    void Start (int thrCnt);
+    void Stop (void);
+    void AddCustomer (ACustomer c);
   private:
     static void bin(unsigned n, int bitsLen);
     static void printBinary(int i, int j, int diff, int mask);
@@ -123,8 +26,6 @@ class CRig
     static void findVarBits(AFITCoin &x);
     static void removeFixBits(AFITCoin &x);
     static void compareWithNumbers(AFITCoin &x);
-    static int min(int x, int y, int z);
-    static uint64_t min(uint64_t x, uint64_t y);
     static int varCount;
     static int testCounter;
     static uint64_t fixCount;
@@ -145,13 +46,6 @@ vector<bool> CRig::boolVector;
 
 
 //--------------------------------------- printing methods -----------------------------------------------
-
-uint64_t CRig::min(uint64_t x, uint64_t y) {
-    if (x < y)
-        return x;
-    else
-        return y;
-}
 
 //via geeksforgeeks
 void CRig::bin(unsigned n, int len) {
@@ -346,7 +240,8 @@ void CRig::Solve (AFITCoin x) {
 }
 
 //--------------------------------------- CVUTCoin methods -----------------------------------------------
-int CRig::min(int x, int y, int z) {
+int min(int x, int y, int z)
+{
     return min(min(x, y), z);
 }
 
@@ -435,6 +330,21 @@ void CRig::Solve (ACVUTCoin x) {
 
 }
 
-#ifndef __PROGTEST__
-#include "test.inc"
-#endif /* __PROGTEST__ */
+CRig::CRig (void) {
+}
+
+CRig::~CRig (void) {
+
+}
+
+void CRig::Start (int thrCnt) {
+
+}
+
+void CRig::Stop (void) {
+
+}
+
+void CRig::AddCustomer (ACustomer c) {
+
+}
